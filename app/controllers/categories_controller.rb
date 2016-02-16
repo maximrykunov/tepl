0fbe5fb6eaf55d1ef2f1e36eb0d82b25d2f2f9c0
Maxim Rykunov <maxim.rykunov@altarix.ru>
2016-02-16 11:03:31 +0300

    Annotate

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  parent_id   :integer
+#  system_name :string
+#  description :text
+#  visible     :boolean          default(TRUE)
+#  position    :integer          default(100)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class CategoriesController < ApplicationController
   def show
     @category = Category.from_param(params[:id])
