8faada851835470ded8b3f83dc121136e42a0f55
Maxim Rykunov <maxim.rykunov@altarix.ru>
2016-02-19 12:39:24 +0300

    Add products

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,8 +1,11 @@
 module ProductHelper
 
   def product_weight(product, extend_count)
-    weight = product.base_weight + extend_count*product.extend_weight
-    "~ #{weight} кг"
+    begin
+      weight = product.base_weight + extend_count*product.extend_weight
+      "~ #{weight} кг"
+    rescue
+    end
   end
 
 end
