0e364e71311d84ab2e380917e78fca963ee01c8c
Maxim Rykunov <maxim.rykunov@altarix.ru>
2016-02-28 19:57:27 +0300

    Fix permit params

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -1,17 +1,5 @@
 ActiveAdmin.register Page do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
+  permit_params :system_name, :title, :meta_keywords, :meta_description, :content
 
 end
