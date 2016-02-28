a0e0a36432448a6d6849346f931d08a96a2820c3
Maxim Rykunov <maxim.rykunov@altarix.ru>
2016-02-28 16:00:03 +0300

    Dotenv conf

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,5 @@ module Tepl
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
-
-    # Load defaults from config/*.env in config
-    Dotenv.load *Dir.glob(Rails.root.join("config/**/*.env"), File::FNM_DOTMATCH)
   end
 end
