8c4737145e33d8260dea203fe20fb88175f0df46
Максим Рыкунов <maxim.rykunov@altarix.ru>
2017-01-23 10:46:38 +0300

    Update meta for article

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -2,6 +2,15 @@ ActiveAdmin.register Article do
 
   permit_params :system_name, :title, :meta_keywords, :meta_description, :content, :visible
 
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :system_name
+    column :visible
+    actions
+  end
+
   form do |f|
     f.inputs do
       f.input :title
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,10 @@ class ArticlesController < InheritedResources::Base
       @category_2 = Category.find_by(system_name: 'teplicy_volya_truba')
       @articles = Article.active.limit(5)
       render '/shared/404', :status => 404
+    else
+      @title = "#{@article.title.gsub('"','')}."
+      @meta_keywords = "#{@article.meta_keywords.gsub('"','')}, #{APP_CONFIG['meta_keywords_tail']}"
+      @meta_description = "#{@article.meta_description.gsub('"','')} #{APP_CONFIG['meta_description_tail']}"
     end
   end
 
