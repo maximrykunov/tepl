1f29a402c9445fbd309b3ee057c520ee0583644b
Максим Рыкунов <maxim.rykunov@altarix.ru>
2017-01-31 15:15:36 +0300

    Play with ckeditor assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery_ujs
 // require turbolinks
 //= require fancybox
+//= require ckeditor/init
 //= require_tree .
 
--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module Tepl
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
 
-    config.assets.precompile += %w(ckeditor/* ckeditor/lang/*)
     config.assets.precompile += Ckeditor.assets
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
