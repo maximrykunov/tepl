a76b69e42ee359ab8f11320c1472bc4cb4485721
Максим Рыкунов <maxim.rykunov@altarix.ru>
2017-01-31 13:38:07 +0300

    Update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :linked_files, %w{.env}
+set :linked_dirs, fetch(:linked_dirs, []).push('public/uploads')
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
